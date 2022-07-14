8efd93c60f603bcd23aeaf08a717ff5fa5b7cc2e
Federico Jure <fjure@atixlabs.com>
2022-07-14 16:47:43 -0300

    se empieza componentes Filter y Products

---
+++ src/componentes/Main/FIlter.jsx
@@ -0,0 +1,13 @@
+import '../../hojas-de-estilo/Styles-Main/Filter.css';
+
+function Filter(){
+    return(
+        <div className="container-filter">
+            <p><a href='https://www.mercadolibre.com.ar/tiendas-oficiales'> Tiendas oficiales</a></p>
+            <p>HP Tienda OFicial</p>
+            <p>238 resultados</p>
+        </div>
+    )
+}
+
+export default Filter;
--- src/componentes/Main/Main.jsx
+++ src/componentes/Main/Main.jsx
@@ -1,13 +1,17 @@
 import "../../hojas-de-estilo/Styles-Main/Main.css";
 import Profile from "../Main/Profile";
+import Filter from "./FIlter";
+import Products from "./Products";
 
 function Main() {
   return (
     <div className="container-main">
       <div className="main-content">
         <Profile />
-        
-        <div className="product-filter"></div>
+        <div className="product-filter">
+            <Filter />
+            <Products />
+        </div>
       </div>
     </div>
   );
