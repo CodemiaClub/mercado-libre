1b448b416f331ac159db0db60ab82cefaca81b08
MNATorres <mtri21019@gmail.com>
2022-07-14 10:39:01 -0300

    se agregan estilos al header

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -4,21 +4,25 @@ import Ubicacion from './Ubicacion';
 import ProductSearch from '../Header/ProductSearch';
 import ProductNav from './ProductNav';
 import Promocion from './Promocion';
+import Usuario from './Usuario';
 
 
 function Header() {
   return (
-    <div className="container-header">
-      <div className="fileOne">
-        <Logo img='mercadolibre' />
-        <Ubicacion />
-      </div>
-      <div className="fileTwo">
-        <ProductSearch />
-        <ProductNav />
-      </div>
-      <div className="fileThree">
-        <Promocion />
+    <div className="Heder">
+      <div className="container-header">
+        <div className="fileOne">
+          <Logo img='mercadolibre' />
+          <Ubicacion />
+        </div>
+        <div className="fileTwo">
+          <ProductSearch />
+          <ProductNav />
+        </div>
+        <div className="fileThree">
+          <Promocion />
+          <Usuario />
+        </div>
       </div>
     </div>
   );
