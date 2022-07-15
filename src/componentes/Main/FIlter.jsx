99a5abaf39a74525281739e00534ef8e20e2b9eb
MNATorres <mtri21019@gmail.com>
2022-07-15 12:03:53 -0300

    se agrega componente Category y se empieza a implementar en componente Filter

--- src/componentes/Main/FIlter.jsx
+++ src/componentes/Main/FIlter.jsx
@@ -1,4 +1,5 @@
 import '../../hojas-de-estilo/Styles-Main/Filter.css';
+import Category from './Category';
 import FilterCard from './FilterCard';
 
 function Filter() {
@@ -10,13 +11,68 @@ function Filter() {
                 <p>238 resultados</p>
             </div>
             <div className="filtercard">
-                <FilterCard 
-                title='Llega mañana'/>
-                <FilterCard 
-                title='te ahorra envíos'
-                text='con tu carrito de compras' />
-                <FilterCard 
-                title='Envío gratis'/>
+                <FilterCard
+                    title='Llega mañana' />
+                <FilterCard
+                    title='te ahorra envíos'
+                    text='con tu carrito de compras' />
+                <FilterCard
+                    title='Envío gratis' />
+            </div>
+            <div className="filter-category">
+                <Category
+                    title='Categorías'
+                    text='Computación'
+                    numb='(12)'
+                    text2='Electrónica, Audio y Video'
+                    numb2='(12)' />
+
+                <Category
+                    title='Costo de envío'
+                    text='Gatis'
+                    numb='(12)' />
+
+                <Category
+                    title='Tiempo de entrega'
+                    text='Llegan hoy'
+                    numb='(12)'
+                    text2='Llegan en menos de 24hs'
+                    numb2='(12)' />
+
+                <Category
+                    title='Precio'
+                    text='Hasta $8.000'
+                    numb='(12)'
+                    text2='$8.000 a $20.000'
+                    numb2='(12)'
+                    text3='Más de $20.000'
+                    numb3='(12)' />
+
+                <Category
+                    title='Ubicación'
+                    text='Capital Federal'
+                    numb='(12)'
+                    text2='Bs.As. G.B.A. Oeste'
+                    numb2='(12)' />
+
+                <Category
+                    title='Pago'
+                    text='Sin Interés'
+                    numb='(12)' />
+
+                <Category
+                    title='Tipo de entrega'
+                    text='Full'
+                    numb='(12)' />
+
+                <Category
+                    title='Marca'
+                    text='HP'
+                    numb='(12)'
+                    text2='Hewlett Packard Enterprise'
+                    numb2='(12)'
+                    text3='(Samsung)'
+                    numb3='(12)' />
             </div>
         </div>
     )
