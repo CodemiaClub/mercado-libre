b464a9791ce085f1b4f7f8ca37a93f09e40e2ab5
MNATorres <mtri21019@gmail.com>
2022-07-17 00:25:20 -0300

    se agrega componente FooterMenu

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,10 +1,55 @@
 import '../../hojas-de-estilo/Styles-Footer/Footer.css';
+import FooterMenu from './FooterMenu';
 import FooterOption from './FooterOption';
 
-function Footer(){
-  return(
+function Footer() {
+  return (
     <div className="container-footer">
       <FooterOption />
+
+      <FooterMenu
+        title='Acerca de'
+        nav1='Mercado Libre'
+        nav2='Investor relations'
+        nav3='Tendencias'
+        nav4='Sustentabilidad' />
+
+      <FooterMenu
+        title='Otros sitios'
+        nav1='Developers'
+        nav2='Mercado Pago'
+        nav3='Mercado Shops'
+        nav4='Mercado Envíos'
+        nav5='Mercado Ads'
+        nav6='Mercado Libre Ideas' />
+
+      <FooterMenu
+        title='Ayuda'
+        nav1='Comprar'
+        nav2='Vender'
+        nav3='Resolución de problemas'
+        nav4='Centro de seguridad' />
+
+      <FooterMenu
+        title='Redes sociales'
+        nav1='Twitter'
+        nav2='Facebook'
+        nav3='Instagram'
+        nav4='YouTube' />
+
+      <FooterMenu
+        title='Mi cuenta'
+        nav1='Ingresá'
+        nav2='Vender' />
+
+      <FooterMenu
+        title='Mercado Puntos'
+        nav1='Nivel 6'
+        nav2='Disney+'
+        nav3='Star+'
+        nav4='HBO MAX'
+        nav5='Paramount+' />
+
     </div>
   )
 }
