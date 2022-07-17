1021f3538cce6ce45cb87a88eb2ed42ab323bf0a
MNATorres <mtri21019@gmail.com>
2022-07-17 12:22:55 -0300

    se termina componente FooterMenu

--- src/componentes/Footer/FooterMenu.jsx
+++ src/componentes/Footer/FooterMenu.jsx
@@ -3,15 +3,13 @@ import '../../hojas-de-estilo/Styles-Footer/FooterMenu.css';
 function FooterMenu({ title, nav1, nav2, nav3, nav4, nav5, nav6 }) {
     return (
         <div className="container-footerMenu">
-            <p>{title}</p>
-            <div className="navFooter">
-                <p>{nav1}</p>
-                <p>{nav2}</p>
-                <p>{nav3}</p>
-                <p>{nav4}</p>
-                <p>{nav5}</p>
-                <p>{nav6}</p>
-            </div>
+            <p className='titleFooter' >{title}</p>
+            <span>{nav1}</span>
+            <span>{nav2}</span>
+            <span>{nav3}</span>
+            <span>{nav4}</span>
+            <span>{nav5}</span>
+            <span>{nav6}</span>
         </div>
     )
 }
