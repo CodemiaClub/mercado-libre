07895bda8115b1b5a3a6338d2691b33252a116f4
MNATorres <mtri21019@gmail.com>
2022-07-17 13:07:21 -0300

    se agregan estilos y links al componente FooterInformation

--- src/componentes/Footer/FooterInformation.jsx
+++ src/componentes/Footer/FooterInformation.jsx
@@ -1,18 +1,18 @@
 import '../../hojas-de-estilo/Styles-Footer/FooterInformation.css';
 
-function FooterInformation(){
-  return(
+function FooterInformation() {
+  return (
     <div className="containerInformation">
       <div className="information">
         <div className="title">
-          <p>Trabajá con nosotros</p>
-          <p>Terminos y condiciones</p>
-          <p>Como cuidamos tu privacidad</p>
-          <p>Accesibilidad</p>
-          <p>Información al usuario financiero</p>
-          <p>Ayuda</p>
-          <p>Defensa del Consumidor</p>
-          <p>Información sobre seguros</p>
+          <a href='https://careers-meli.mercadolibre.com/?utm_campaign=site-mla&utm_source=mercadolibre&utm_medium=mercadolibre'><p>Trabajá con nosotros</p></a>
+          <a href='https://www.mercadolibre.com.ar/ayuda/terminos-y-condiciones-de-uso_991'><p>Terminos y condiciones</p></a>
+          <a href='https://www.mercadolibre.com.ar/privacidad'><p>Como cuidamos tu privacidad</p></a>
+          <a href='https://www.mercadolibre.com.ar/accesibilidad'><p>Accesibilidad</p></a>
+          <a href='https://www.mercadolibre.com.ar/ayuda/18697'><p>Información al usuario financiero</p></a>
+          <a href='https://www.mercadolibre.com.ar/ayuda'><p>Ayuda</p></a>
+          <a href='https://www.mercadolibre.com.ar/ayuda/Defensa-del-Consumidor_s20014'><p>Defensa del Consumidor</p></a>
+          <a href='https://www.mercadolibre.com.ar/ayuda/23303'><p>Información sobre seguros</p></a>
         </div>
         <div className="legal">
           <p>Copyright © 1999-2022 MercadoLibre S.R.L.</p>
@@ -20,7 +20,7 @@ function FooterInformation(){
         </div>
       </div>
     </div>
-  )    
+  )
 }
 
 export default FooterInformation;
